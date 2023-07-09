9b287a4a200affd526dbcd016144af34a75e4acb
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-07-09 00:09:28 -0400

    add entities

---
+++ src/main/java/com/minhhoangtran/springjpa/entity/Grade.java
@@ -0,0 +1,23 @@
+package com.minhhoangtran.springjpa.entity;
+
+public class Grade {
+    private Long id;
+    private String score;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getScore() {
+        return this.score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+}
