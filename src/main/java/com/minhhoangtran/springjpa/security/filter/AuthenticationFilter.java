475eb9eea87cffe58fa0ecca2285d09bf44cfb97
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-07-14 13:12:42 -0400

    add jwt authentication to authentication manager

--- src/main/java/com/minhhoangtran/springjpa/security/filter/AuthenticationFilter.java
+++ src/main/java/com/minhhoangtran/springjpa/security/filter/AuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.minhhoangtran.springjpa.security.filter;
 
 import java.io.IOException;
+import java.util.Date;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -8,10 +9,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minhhoangtran.springjpa.entity.User;
+import com.minhhoangtran.springjpa.security.SecurityConstants;
 import com.minhhoangtran.springjpa.security.manager.CustomAuthenticationManager;
 
 import jakarta.servlet.FilterChain;
@@ -41,8 +45,18 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
             Authentication authResult) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        super.successfulAuthentication(request, response, chain, authResult);
+        String token = JWT.create().withSubject(authResult.getName())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET_KEY));
+        response.addHeader(SecurityConstants.AUTHORIZATION, SecurityConstants.BEARER + token);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("Authentication failed");
+        response.getWriter().flush();
     }
 
 }
