10f826952869df37bae243971d0eca53add53f42
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-07-14 02:53:42 -0400

    Update ExceptionHandlerFilter.java

--- src/main/java/com/minhhoangtran/springjpa/security/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/minhhoangtran/springjpa/security/filter/ExceptionHandlerFilter.java
@@ -13,8 +13,11 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+        try {
+            filterChain.doFilter(request, response);
+        } catch (RuntimeException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
 
 }
